e962df0ebe70c42ffd5c71ef5c95ce611eee836e
sharon <sharonoptsout@gmail.com>
2018-05-10 23:29:36 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,10 @@
 // declare your functions here...
+function paragraphSelector() {$('p').return();}
+
+function lastImageSelector() {}
+
+function ninjaBabySelector() {}
+
+function divSelector() {}
+
+function firstListItem() {}
